360f5c0947b6ca964eb36359e03fcdc7dc17b286
Rojorom <58658571+Rojorom@users.noreply.github.com>
2020-06-04 18:53:24 -0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -10,7 +10,7 @@ function writing(str){
   let i = 0;
   let printStr = setInterval(function(){
 
-   document.body.style.fontSize = '';
+  //  document.body.style.fontSize = '';
    document.querySelector('.text').innerHTML += arrFromStr[i];
    i++;
    if (i === arrFromStr.length){
@@ -23,7 +23,22 @@ function writing(str){
  writing('Ve unos de los proyectos personales. "La gente Genial desarrolla con Nosotros " ');
 
 
+ function writingII(str){
+  let arrFromStr = str.split('');   //split tranforma el string en Array
+  let i = 0;
+  let printStr = setInterval(function(){
 
+   document.body.style.fontSize = '';
+   document.querySelector('.slogan').innerHTML += arrFromStr[i];
+   i++;
+   if (i === arrFromStr.length){
+    clearInterval(printStr);
+    // document.body.style.fontSize = '60px';
+    // document.body.style.color = 'steelblue';
+   }
+  },70);
+ };
+ writingII('solicita tu pagina 100% personalizada y adaptable a cualquier dispositivo o  cualquier aplicacion al mejor precio del mercado ;) " ');
 
 
 
